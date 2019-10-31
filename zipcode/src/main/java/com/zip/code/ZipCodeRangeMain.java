ad482ed934f0a9a308d9db5cf4aec0464a211907
terrencewuxl1 <terrencewuxl@gmail.com>
2019-10-31 07:50:44 -0700

    format code

--- zipcode/src/main/java/com/zip/code/ZipCodeRangeMain.java
+++ zipcode/src/main/java/com/zip/code/ZipCodeRangeMain.java
@@ -9,36 +9,37 @@ import static java.lang.Integer.parseInt;
 public class ZipCodeRangeMain {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         // using args to input zip code ranges, each range as a arg by [number,number] format
-        if(args.length<1){
+        if (args.length < 1) {
             System.out.println("Usage:java -jar <jar name> <zip range> <zip range>...");
             System.out.println(" <zip range> format : [number,number]");
             return;
         }
-        int[][] inputArray=convertStringToIntArray(args);
-        if(inputArray==null)return;
-        int[][] result=new ZipCodeRangeUtil().findMinNumOfZipRanges(inputArray);
+        int[][] inputArray = convertStringToIntArray(args);
+        if (inputArray == null) return;
+        int[][] result = new ZipCodeRangeUtil().findMinNumOfZipRanges(inputArray);
         System.out.println(Arrays.deepToString(result));
     }
 
 
     /**
-     *  convert args to int[] array, args must be [number,number] [number,number]...</br>
-     *  format, otherwise, return null.
+     * convert args to int[] array, args must be [number,number] [number,number]...</br>
+     * format, otherwise, return null.
+     *
      * @param args program arguments, input the zip ranges string array
      * @return int[] array contains zip ranges
      */
     private static int[][] convertStringToIntArray(String[] args) {
-        int[][] zipIntArray=new int[args.length][2];
-        for(int i=0;i<args.length;i++){
-            String[] numbers=args[i].substring(1,args[i].length()-1)
+        int[][] zipIntArray = new int[args.length][2];
+        for (int i = 0; i < args.length; i++) {
+            String[] numbers = args[i].substring(1, args[i].length() - 1)
                     .split(",");
-            try{
-                zipIntArray[i][0]= parseInt(numbers[0]);
-                zipIntArray[i][1]= parseInt(numbers[1]);
-            }catch (NumberFormatException e){
+            try {
+                zipIntArray[i][0] = parseInt(numbers[0]);
+                zipIntArray[i][1] = parseInt(numbers[1]);
+            } catch (NumberFormatException e) {
                 //If the args format incorrect, print usage message.
                 System.out.println("Zip range format error, <zip range> format : [number,number]");
                 return null;
